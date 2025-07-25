6b741b6bb428e9f9b3dd1aa4e0ea5dbe5fdb4b3c
Durdana_Khalid <durdanakhalid98@gmail.com>
2025-07-25 09:25:54 +0500

    Update app.py

--- Flask_Web_App/app.py
+++ Flask_Web_App/app.py
@@ -17,7 +17,7 @@ with tempfile.NamedTemporaryFile(delete=False) as tmp:
     tmp.write(response.content)
     model = joblib.load(tmp.name)
 # Load the dataset (ensure you have the correct path)
-df = pd.read_csv("D:/Portfolio Projects/Climate Trend Analysis – Monthly Temperature Patterns/Data_/average_monthly_temperature_by_state_1950-2022.csv")
+df = pd.read_csv("Data_/average_monthly_temperature_by_state_1950-2022.csv")
 # Drop Unnecessary column
 df.drop(columns=['Unnamed: 0'], inplace=True)
 
